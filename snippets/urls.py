cf5bf8a9b2a36a65a76f28fcf84bd5bcc94edaab
samke-glitch <sammythemwa@gmail.com>
2024-10-17 16:11:24 +0300

    response objects

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('', views.snippet_list),  # This matches /snippets/
     path('<int:pk>/', views.snippet_detail),  # This matches /snippets/<id>/
 ]
-
+urlpatterns = format_suffix_patterns(urlpatterns)
