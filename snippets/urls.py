5b06d90f71a548b6ade1c571291d575d085dba3b
samke-glitch <sammythemwa@gmail.com>
2024-10-21 11:23:10 +0300

    new: ruls

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,5 +5,7 @@ from snippets import views
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),  # This matches /snippets/
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),  # This matches /snippets/<id>/
+    path('users/', views.UserList.as_view()), # User list view
+    path('users/<int:pk>/', views.UserDetail.as_view()), # User detail view
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
