a8f427c48c30bf2cbe10d601c2a562fbd6e8821a
samke-glitch <sammythemwa@gmail.com>
2024-10-21 11:55:49 +0300

    updating serializer

--- snippets/views.py
+++ snippets/views.py
@@ -9,6 +9,10 @@ class SnippetList(generics.ListCreateAPIView):
     serializer_class = SnippetSerializer
 
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
