b8173d63daadcde71c51d063493344b282151f33
samke-glitch <sammythemwa@gmail.com>
2024-10-21 11:19:47 +0300

    new: update views

--- snippets/views.py
+++ snippets/views.py
@@ -1,8 +1,9 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer  # Import SnippetSerializer and UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User  # Import the User model
 
-
+# Snippet views
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -11,3 +12,14 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+# User views
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
